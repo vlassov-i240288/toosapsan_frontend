f9d97ed61ad118a22b993b610991d684310aefe7
Vlasov_I <vlasov.i.88@gmail.com>
2025-08-18 18:29:08 +0500

    modal v.1

--- src/components/sections/header/Header.jsx
+++ src/components/sections/header/Header.jsx
@@ -1,18 +1,18 @@
-import {
-    HiOutlineLocationMarker,
-    HiOutlineSwitchHorizontal,
-    HiOutlineCube,
-    HiOutlineScale,
-    HiOutlineUser,
-    HiPhone
-} from "react-icons/hi";
-
 import { Bs1Circle, Bs2Circle, Bs3Circle, Bs4Circle } from "react-icons/bs";
 import { useState } from "react";
+import AplicationModal from "../../modals/ApplicationModal";
 
 export default function Header() {
+    // const handleOrder = (type) => {
+    //     console.log(`Заказ: ${type}`);
+    // };
+
+    const [openModal, setOpenModal] = useState(false);
+    const [orderType, setOrderType] = useState("");
+
     const handleOrder = (type) => {
-        console.log(`Заказ: ${type}`);
+        setOrderType(type);
+        setOpenModal(true);
     };
 
     return (
@@ -41,74 +41,12 @@ export default function Header() {
                         <button className="border border-amber-400 p-3" onClick={() => handleOrder("Ж/Д перевозка")}>Заказать Ж/Д перевозку</button>
                     </div>
                 </div>
-
-                <div className="">
-                    <form action="" className='border border-amber-400 p-5 rounded'>
-                        <div className='flex flex-col gap-2'>
-                            <p className='text-white text-2xl font-bold'>Данные о грузе :</p>
-                            <div className='flex flex-col gap-2'>
-
-                                <div className='relative'>
-                                    <div className='absolute top-[10px] left-[10px] '>
-                                        <HiOutlineLocationMarker size={25} color="oklch(76.9% 0.188 70.08)" />
-                                    </div>
-                                    <input className='bg-amber-50 p-3 ps-10 w-full rounded text-gray-800' type="text" placeholder='Откуда' />
-                                </div>
-
-
-                                <div className='flex w-10 h-10 cursor-pointer items-center self-center'>
-                                    <HiOutlineSwitchHorizontal size={25} color="oklch(76.9% 0.188 70.08)" />
-                                </div>
-
-                                <div className='relative'>
-                                    <div className='absolute top-[10px] left-[10px]'>
-                                        <HiOutlineLocationMarker size={25} color="oklch(76.9% 0.188 70.08)" />
-                                    </div>
-                                    <input className='bg-amber-50 p-3 ps-10 w-full rounded text-gray-800' type="text" placeholder='Куда' />
-                                </div>
-                            </div>
-                            <div className='flex flex-col gap-2'>
-                                <div className='relative'>
-                                    <div className='absolute top-[10px] left-[10px]'>
-                                        <HiOutlineCube size={25} color="oklch(76.9% 0.188 70.08)" />
-                                    </div>
-
-                                    <input className='bg-amber-50 p-3 ps-10 w-full rounded text-gray-800' type="number" placeholder='Объём' />
-                                </div>
-                                <div className='relative'>
-                                    <div className='absolute top-[10px] left-[10px]'>
-                                        <HiOutlineScale size={25} color="oklch(76.9% 0.188 70.08)" />
-                                    </div>
-                                    <input className='bg-amber-50 p-3 ps-10 w-full rounded text-gray-800' type="number" placeholder='Вес' />
-                                </div>
-                            </div>
-                            <p className='text-white text-2xl font-bold'>Контактные данные :</p>
-                            <div className='flex flex-col gap-2'>
-
-                                <div className='relative'>
-                                    <div className='absolute top-[10px] left-[10px]'>
-                                        <HiOutlineUser size={25} color="oklch(76.9% 0.188 70.08)" />
-                                    </div>
-                                    <input className='bg-white p-3 ps-10 w-full rounded text-gray-800' type="text" placeholder='Имя' />
-                                </div>
-
-                                <div className='relative'>
-                                    <div className='absolute top-[10px] left-[10px]'>
-                                        <HiPhone size={25} color="oklch(76.9% 0.188 70.08)" />
-                                    </div>
-                                    <input className='bg-white p-3 ps-10 w-full rounded text-gray-800' type="text" placeholder='Номер телефона' />
-                                </div>
-                            </div>
-
-                            <button className="bg-amber-400 rounded p-3 cursor-pointer hover:bg-amber-500 transition-colors duration-300 text-gray-800">
-                                Расчитать стоимость
-                            </button>
-                        </div>
-                    </form>
-
-                </div>
-
             </div>
+
+            {/* модалка */}
+            {openModal && (
+                <AplicationModal onClose={() => setOpenModal(false)}/>
+            )}
         </header>
     )
 }
