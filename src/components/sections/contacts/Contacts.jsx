a494d89a529df65a3de500ab297f507e1e6fbe0a
Vlasov_I <vlasov.i.88@gmail.com>
2025-11-05 10:35:19 +0500

    add Footer v.3

--- src/components/sections/contacts/Contacts.jsx
+++ src/components/sections/contacts/Contacts.jsx
@@ -124,7 +124,7 @@ export default function Contacts() {
                 <iframe
                     src="https://yandex.ru/map-widget/v1/?um=constructor%3Ad4e2611309451076324e730a7ac84643c5330214c9208ebe21c5c840284bfa45&amp;source=constructor"
                     width="100%"
-                    height="720"
+                    height="450"
                     frameBorder="0"
                 ></iframe>
             </div>
